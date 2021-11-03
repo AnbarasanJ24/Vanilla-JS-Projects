e67923bd84ac57f94b1bdf75f39e9a2f0a500504
Anbarasan <anbarasanj24@gmail.com>
2021-11-03 22:02:33 +0530

    Completed Quote Generator Application

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -1,20 +1,67 @@
+const quoteContainer = document.querySelector('.quote-container');
+const quoteText = document.querySelector('.quote-text');
+const authorText = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newQuoteBtn = document.querySelector('#new-quote');
+const loader = document.querySelector('#loader');
 
 
-
-// Get Quotes
 const URL = "https://type.fit/api/quotes";
-let quote = [];
+let quotes = [];
+
+function newQuote() {
+    showLoader();
+    const index = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[index];
+
+    author.textContent = quote.author || 'Unknown';
+
+    // check Quote Length
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    hideLoader();
+}
 
 async function getQuotes() {
+    showLoader();
     try {
         const data = await fetch(URL);
-        const quotes = await data.json();
-        console.log(quotes);
+        quotes = await data.json();
+        newQuote();
+        // console.log(quotes);
     } catch (err) {
         console.log("Error", err);
     }
+    hideLoader();
 }
 
+
+function tweetQuote() {
+    const twitterURL = `https://twitter.com/intent/tweet/?text=${quoteText.textContent}- ${authorText.textContent}`;
+    window.open(twitterURL, '_blank');
+}
+
+
+function showLoader() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function hideLoader() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+
+
 // On Load
-// window.addEventListener()
 getQuotes();
