3e091b558ed054b9e64c0bfc7b05f30c221adb5f
Anbarasan <anbarasanj24@gmail.com>
2021-11-03 09:46:24 +0530

    "Laoded Quotes from External API"

--- Quote-Generator/script.js
+++ Quote-Generator/script.js
@@ -1,1 +1,20 @@
-console.log("Template Check")
+
+
+
+// Get Quotes
+const URL = "https://type.fit/api/quotes";
+let quote = [];
+
+async function getQuotes() {
+    try {
+        const data = await fetch(URL);
+        const quotes = await data.json();
+        console.log(quotes);
+    } catch (err) {
+        console.log("Error", err);
+    }
+}
+
+// On Load
+// window.addEventListener()
+getQuotes();
