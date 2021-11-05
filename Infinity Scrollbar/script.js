868cb6f1920bfa6be79ac46108b0b0001add6ec1
Anbarasan <anbarasanj24@gmail.com>
2021-11-05 22:14:51 +0530

    Added Inifinity Scrollbar

--- Infinity Scrollbar/script.js
+++ Infinity Scrollbar/script.js
@@ -1,1 +1,17 @@
-console.log("Template Check")
+const imageCount = 2;
+const API_KEY = "KXSv44wiKDOia4q_0rsZeL5TK28GyQ89c8KUFemAMQs";
+const URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${imageCount}`;
+
+async function getPhotos() {
+    try {
+        const response = await fetch(URL);
+        const images = await response.json();
+        console.log("image ", images);
+
+    } catch (err) {
+        console.log("Error", err);
+    }
+
+}
+
+getPhotos();
