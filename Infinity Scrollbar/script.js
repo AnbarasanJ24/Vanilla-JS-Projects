14badd69782c78687476a18e78a9b5afbeadc43c
Anbarasan <anbarasanj24@gmail.com>
2021-11-06 09:56:29 +0530

    Added Intersection observer

--- Infinity Scrollbar/script.js
+++ Infinity Scrollbar/script.js
@@ -1,12 +1,12 @@
 const imageContainer = document.querySelector('#image-container');
-const images = document.querySelectorAll('img');
-let photos = [];
+
+let data = [];
 let ready = false;
 let totalImages = 0;
 let loadedImages = 0;
 
 
-const imageCount = 5;
+const imageCount = 2;
 const API_KEY = "KXSv44wiKDOia4q_0rsZeL5TK28GyQ89c8KUFemAMQs";
 const URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${imageCount}`;
 
@@ -26,9 +26,9 @@ function setAttributes(element, attributes) {
 
 function displayPhotos() {
     loadedImages = 0;
-    totalImages = photos.length;
+    totalImages = data.length;
 
-    photos.forEach(photo => {
+    data.forEach(photo => {
 
         // Anchor Tag
         const aTag = document.createElement('a');
@@ -56,7 +56,7 @@ function displayPhotos() {
 async function getPhotos() {
     try {
         const response = await fetch(URL);
-        photos = await response.json();
+        data = await response.json();
         displayPhotos();
     } catch (err) {
         console.log("Error", err);
@@ -76,3 +76,61 @@ getPhotos();
 
 
 // Intersection Observer Approach 
+
+
+
+function createImageElement(photo) {
+    // Anchor Tag
+    const aTag = document.createElement('a');
+    setAttributes(aTag, {
+        'href': photo.links.html,
+        'target': '_blank'
+    })
+
+    // Image Tag
+    const img = document.createElement('img');
+    setAttributes(img, {
+        'data-src': photo.urls.regular,
+        'alt': photo.alt_description,
+        'title': photo.alt_description
+    });
+
+    // Image container> (a > img)
+    aTag.appendChild(img);
+    imageContainer.appendChild(aTag);
+}
+
+function preloadImage(image) {
+    const src = image.getAttribute("data-src");
+    if (!src) return;
+
+    image.src = src;
+}
+const imageOptions = {
+    threshold: 0.2
+}
+const imageObserver = new IntersectionObserver((entries, imageObserver) => {
+    console.log(entries)
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log("Entry", entry);
+            preloadImage(entry.target);
+            imageObserver.unobserve(entry.target);
+        }
+    })
+}, imageOptions);
+
+// async function getPhotos() {
+//     try {
+//         const response = await fetch(URL);
+//         data = await response.json();
+//         data.forEach(createImageElement);
+//         const images = document.querySelectorAll("img");
+//         images.forEach(image => imageObserver.observe(image));
+//     } catch (err) {
+//         console.log("Error", err);
+//     }
+
+// }
+
+// getPhotos();
